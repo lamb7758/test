fbb72140dd2447f6f3a8be0cf2ca6dd21bb551cb
gaoyang <lamb7758.com>
2019-03-15 21:41:59 +0800

    copy sout

--- src/main/java/com/lamb/Test.java
+++ src/main/java/com/lamb/Test.java
@@ -3,5 +3,8 @@ package com.lamb;
 public class Test {
     public static void main(String[] args) {
         System.out.println("lamb7758");
+        System.out.println("lamb7758");
+        System.out.println("lamb7758");
+        System.out.println("lamb7758");
     }
 }

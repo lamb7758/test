37dcc5846db7e9cb6fbb630d8726c3ca8f2ffe9b
gaoyang <lamb7758.com>
2019-03-15 22:04:37 +0800

    Merge test
    Signed-off-by: gaoyang <lamb7758.com>

--- src/main/java/com/lamb/Test.java
+++ src/main/java/com/lamb/Test.java
@@ -3,10 +3,6 @@ package com.lamb;
 public class Test {
     public static void main(String[] args) {
         System.out.println("lamb7758");
-        System.out.println("lamb7758");
-        System.out.println("lamb7758");
-        System.out.println("lamb7758");
-        System.out.println("lamb7758");
-        System.out.println("lamb7758");
+       
     }
 }
